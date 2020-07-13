197966cd1247daa520a2c7b6d212484167d9092c
Adrian Pionka <adrian@pionka.com>
2020-07-13 21:29:03 +0200

    Added network module.

---
+++ app/src/main/java/pl/pionas/kotlinaplication/core/MainActivity.kt
@@ -0,0 +1,8 @@
+package pl.pionas.kotlinaplication.core
+
+/**
+ * Created by Adrian Pionka on 13 lipiec 2020
+ * adrian@pionka.com
+ */
+class MainActivity {
+}
---
+++ app/src/main/java/pl/pionas/kotlinaplication/fatures/data/RickAndMortyApi.kt
@@ -0,0 +1,8 @@
+package pl.pionas.kotlinaplication.fatures.data
+
+/**
+ * Created by Adrian Pionka on 13 lipiec 2020
+ * adrian@pionka.com
+ */
+interface RickAndMortyApi {
+}
