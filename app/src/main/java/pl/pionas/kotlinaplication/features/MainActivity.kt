6047cf1fb81b48e2cb787d6cfc7b89b0c34cb7dc
Adrian Pionka <adrian@pionka.com>
2020-08-31 13:56:58 +0200

    add GetAuthUseCaseTest

--- app/src/main/java/pl/pionas/kotlinaplication/features/MainActivity.kt
+++ app/src/main/java/pl/pionas/kotlinaplication/features/MainActivity.kt
@@ -7,16 +7,18 @@ import pl.pionas.kotlinaplication.R
 import pl.pionas.kotlinaplication.features.characters.presentation.CharacterFragment
 import pl.pionas.kotlinaplication.features.episodes.presentation.EpisodeFragment
 import pl.pionas.kotlinaplication.features.locations.presentation.LocationFragment
+import pl.pionas.kotlinaplication.features.users.login.presentation.LoginFragment
 
 class MainActivity : AppCompatActivity() {
 
     private val episodeFragment: EpisodeFragment by inject()
     private val characterFragment: CharacterFragment by inject()
     private val locationFragment: LocationFragment by inject()
+    private val loginFragment: LoginFragment by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        supportFragmentManager.beginTransaction().add(R.id.mainLayout, episodeFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.mainLayout, loginFragment).commit()
     }
 }
